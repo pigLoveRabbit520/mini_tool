3a0a226b6e8c2662da6ae4d04014dd221242c45f
pig <1906747819@qq.com>
2023-03-30 17:29:09 +0800

    增加了获取CPU的信息，但是编译有问题。

--- src/mainboard.cpp
+++ src/mainboard.cpp
@@ -34,3 +34,131 @@ int MainBoard::GetInfo()
     RegCloseKey(hKey);
     return 0;
 }
+
+int MainBoard::getCPUInfo()
+{
+    HRESULT hres;
+
+    // 初始化COM库
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres))
+    {
+        std::cerr << "Failed to initialize COM library. Error code: 0x" << std::hex << hres << std::endl;
+        return errFailed;
+    }
+
+    // 设置COM安全性
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,
+        NULL,
+        NULL,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL,
+        EOAC_NONE,
+        NULL
+    );
+    if (FAILED(hres))
+    {
+        std::cerr << "Failed to initialize security. Error code: 0x" << std::hex << hres << std::endl;
+        CoUninitialize();
+        return errFailed;
+    }
+
+    // 获取WMI服务对象
+    IWbemLocator* pLoc = NULL;
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator,
+        reinterpret_cast<LPVOID*>(&pLoc)
+    );
+    if (FAILED(hres))
+    {
+        std::cerr << "Failed to create IWbemLocator object. Error code: 0x" << std::hex << hres << std::endl;
+        CoUninitialize();
+        return errFailed;
+    }
+
+    IWbemServices* pSvc = NULL;
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        NULL,
+        NULL,
+        0,
+        NULL,
+        0,
+        0,
+        &pSvc
+    );
+    if (FAILED(hres))
+    {
+        std::cerr << "Failed to connect to IWbemServices object. Error code: 0x" << std::hex << hres << std::endl;
+        pLoc->Release();
+        CoUninitialize();
+        return errFailed;
+    }
+
+    // 设置WMI查询语句
+    IEnumWbemClassObject* pEnumerator = NULL;
+    hres = pSvc->ExecQuery(
+        _bstr_t("WQL"),
+        _bstr_t("SELECT Name, Manufacturer, MaxClockSpeed FROM Win32_Processor"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        NULL,
+        &pEnumerator
+    );
+    if (FAILED(hres))
+    {
+        std::cerr << "Failed to execute WQL query. Error code: 0x" << std::hex << hres << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return errFailed;
+    }
+
+    // 解析查询结果
+    IWbemClassObject* pclsObj = NULL;
+    ULONG uReturn = 0;
+    while (pEnumerator)
+    {
+        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        if (uReturn == 0)
+        {
+            break;
+        }
+
+        VARIANT vtProp;
+        hres = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
+        if (SUCCEEDED(hres))
+        {
+            this->CPUName = vtProp.bstrVal;
+            VariantClear(&vtProp);
+        }
+
+        hres = pclsObj->Get(L"Manufacturer", 0, &vtProp, 0, 0);
+        if (SUCCEEDED(hres))
+        {
+            this->CPUManufacturer = vtProp.bstrVal;
+            VariantClear(&vtProp);
+        }
+
+        hres = pclsObj->Get(L"MaxClockSpeed", 0, &vtProp, 0, 0);
+        if (SUCCEEDED(hres))
+        {
+            this->CPUMaxClockSpeed = vtProp.ulVal;
+            VariantClear(&vtProp);
+        }
+
+        pclsObj->Release();
+    }
+
+    // 释放资源
+    pSvc->Release();
+    pLoc->Release();
+    pEnumerator->Release();
+    CoUninitialize();
+    return 0;
+}
--- src/mainboard.h
+++ src/mainboard.h
@@ -2,7 +2,12 @@
 #define MAINBOARD_H
 #include <wx/wx.h>
 #include <windows.h>
+#include <comdef.h>
+#include <comutil.h>
+#include <Wbemidl.h>
 
+#pragma comment(lib, "wbemuuid.lib")
+#pragma comment(lib, "comsuppw.lib")
 #pragma comment(lib, "Advapi32.lib")
 
 class MainBoard
@@ -11,8 +16,13 @@ public:
     int GetInfo();
     wxString BaseBoardManufacturer; // 主板制造商
     wxString BaseBoardProduct; // 主板型号
+    wxString CPUName; // CPU名称
+    wxString CPUManufacturer; // CPU制造商
+    long CPUMaxClockSpeed; // CPU主频
 private:
     const int errFailed = -1;
+
+    int getCPUInfo();
 };
 
 #endif
